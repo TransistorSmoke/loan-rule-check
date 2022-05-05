54d27edffcaa0dd31ad6e86687d3849610353b59
Jason <jmaalegado@gmail.com>
2022-05-05 11:38:21 +1000

    created repayment table

---
+++ src/components/LoanCalculationsTable.vue
@@ -0,0 +1,100 @@
+<template>
+  <h1>Loan Repayment Result</h1>
+  <div class="tbl-container">
+    <table>
+      <thead>
+        <tr>
+          <th colspan="2" class="rule">
+            LENDER RULE
+            <span class="rule-type">{{ this.monthlyRepaymentData?.rule }}</span>
+          </th>
+          <th>Repayment ($)</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Loan Amount ($)</td>
+          <td>{{ this.monthlyRepaymentData?.amount }}</td>
+          <td rowspan="3" class="amt-repayment">
+            {{ this.monthlyRepaymentData?.amountRepayment.toFixed(2) }}
+          </td>
+        </tr>
+        <tr>
+          <td>Loan Term (Years)</td>
+          <td>{{ this.monthlyRepaymentData?.paymentPeriod }}</td>
+        </tr>
+        <tr>
+          <td>Loan Amount (Months)</td>
+          <td>
+            {{
+              this.monthlyRepaymentData?.paymentPeriod
+                ? this.monthlyRepaymentData?.paymentPeriod * 12
+                : ""
+            }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoanCalculationsTable",
+  data() {
+    return {
+      monthlyRepaymentData: null,
+    };
+  },
+  mounted() {
+    this.emitter.on("emit-repayment", (repaymentData) => {
+      console.log(repaymentData);
+      this.monthlyRepaymentData = repaymentData;
+    });
+  },
+};
+</script>
+<style lang="scss" scoped>
+.tbl-container {
+  font-size: 0.9rem;
+  font-weight: bold;
+  max-width: 90%;
+  margin: 0 auto;
+
+  table {
+    border-collapse: collapse;
+    margin: 0 auto;
+  }
+
+  tr,
+  th,
+  td {
+    border: 1px solid #ccc;
+    width: 50%;
+    padding: 4px 8px 4px 0;
+  }
+
+  tr td:first-child {
+    text-align: right;
+  }
+
+  td:first-child,
+  td:nth-child(2) {
+    width: 35%;
+  }
+
+  .rule {
+    font-size: 1rem;
+    background-color: rgb(238, 238, 238);
+
+    &-type {
+      color: rgb(214, 49, 49);
+      text-decoration: underline;
+    }
+  }
+  .amt-repayment {
+    background-color: rgb(255, 212, 212);
+    font-size: 1rem;
+  }
+}
+</style>
---
+++ src/services/HelperService.js
@@ -0,0 +1,51 @@
+import PMT from "formula-pmt";
+
+/*
+ *   Distinct rules for Lender A and lender B, where:
+ *     - Lender A has 5.5% lender interest rate p.a., $40 monthly fee and 2% broker fee p.a.
+ *     - Lender A has 6% lender interest rate p.a., $45 monthly fee and 1.7% broker fee p.a.
+ */
+
+const lenderRuleA = {
+  interestRatePerAnnum: 0.055,
+  monthlyFee: 40,
+  brokerFeePerAnnum: 0.02,
+};
+
+const lenderRuleB = {
+  interestRatePerAnnum: 0.06,
+  monthlyFee: 50,
+  brokerFeePerAnnum: 0.017,
+};
+
+export default {
+  getPmtValue(rule, paymentPeriods, principalValue) {
+    let interestRate = null;
+
+    if (rule === "A") {
+      interestRate = lenderRuleA.interestRatePerAnnum;
+    } else if (rule === "B") {
+      interestRate = lenderRuleB.interestRatePerAnnum;
+    }
+
+    return PMT(interestRate / 12, paymentPeriods * 12, principalValue);
+  },
+
+  getMonthlyRepayment(ruleType, PMT, paymentPeriods) {
+    let monthlyRepayment = null;
+
+    if (ruleType && ruleType === "A" && PMT) {
+      monthlyRepayment =
+        (PMT - lenderRuleA.monthlyFee) *
+        // eslint-disable-next-line prettier/prettier
+        (1 + (lenderRuleA.brokerFeePerAnnum / (paymentPeriods * 12)));
+    } else if (ruleType && ruleType === "B" && PMT) {
+      monthlyRepayment =
+        PMT +
+        // eslint-disable-next-line prettier/prettier
+        ((1 + (lenderRuleB.brokerFeePerAnnum / (paymentPeriods * 12))) - lenderRuleB.monthlyFee);
+    }
+
+    return monthlyRepayment;
+  },
+};
